666b03351b84fcc6dd91c0e658248dbd21611bd2
Parvesh93 <141941315+Parvesh93@users.noreply.github.com>
2026-09-20 06:27:54 +0530

    Fix application filter layout

--- resources/views/admin/application/index.blade.php
+++ resources/views/admin/application/index.blade.php
@@ -36,7 +36,7 @@
                                     </div>
                                 </div>  --}}
                                 
-                                 <div class="form-group col-md-3">
+                                 <div class="form-group col-md-2">
                                         <label for="department">{{ 'College Department' }}</label>
                                         <select class="form-control department" name="department" id="department" required>
                                             <option value="0">{{ __('all') }}</option>
@@ -78,16 +78,16 @@
                                     <div class="invalid-feedback">
                                       {{ __('required_field') }} {{ __('field_status') }}
                                     </div>
-                                <div class="form-group col-md-3">
-                                    <label for="person">Reference Person Name</label>
+                                </div>
+
+                                <div class="form-group col-md-2">
+                                    <label for="person">Reference Person</label>
                                     <select class="form-control" name="person" id="person">
                                         <option value="0">{{ __('all') }}</option>
                                         @foreach ($persons as $person)
                                             <option value="{{ $person }}" @if($selected_person == $person) selected @endif>{{ $person }}</option>
                                         @endforeach
                                     </select>
-                                </div>
-
                                 </div>
                                 <div class="form-group col-md-2">
                                     <label for="start_date">{{ __('field_from_date') }}</label>
